6b0a6e6ed142bdc1a9a51c7a8eeae49c7545a4f1
webflyx <zavala180395@gmail.com>
2023-11-06 18:56:18 +0200

    Added Auth senctum. Login/Logout logic

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,8 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// Route::post('login', [AuthController::class,'login']);
-// Route::post('logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
+Route::post('login', [AuthController::class,'login']);
+Route::post('logout', [AuthController::class,'logout'])->middleware('auth:sanctum');
 
 // Route::apiResource('events', EventController::class);
 
